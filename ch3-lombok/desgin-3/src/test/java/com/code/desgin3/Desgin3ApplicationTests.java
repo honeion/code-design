b7dd2894c7a553f51300a44a278d04b293a6bb0e
honeion <honeion2012@gmail.com>
2023-05-16 00:34:05 +0900

    #3 init 기본 셋팅

---
+++ ch3-lombok/desgin-3/src/test/java/com/code/desgin3/Desgin3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.desgin3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desgin3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
