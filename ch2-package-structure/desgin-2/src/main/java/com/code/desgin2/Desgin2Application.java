ac96c95655fd7e3074357f7a42a022174dff2614
honeion <honeion2012@gmail.com>
2023-05-15 23:26:24 +0900

    #1 기본셋팅

---
+++ ch2-package-structure/desgin-2/src/main/java/com/code/desgin2/Desgin2Application.java
@@ -0,0 +1,13 @@
+package com.code.desgin2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desgin2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desgin2Application.class, args);
+	}
+
+}
