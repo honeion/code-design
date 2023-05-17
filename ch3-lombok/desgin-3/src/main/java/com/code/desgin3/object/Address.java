3ec5ad94bd0d3c9a9d31e103a3c684a2bb366cb6
honeion <honeion2012@gmail.com>
2023-05-17 23:49:03 +0900

    #3 Object 관련

---
+++ ch3-lombok/desgin-3/src/main/java/com/code/desgin3/object/Address.java
@@ -0,0 +1,36 @@
+package com.code.desgin3.object;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Address {
+
+    @Column(name = "address1", nullable = false)
+    private String address1;
+
+    @Column(name = "address2", nullable = false)
+    private String address2;
+
+    @Column(name = "zip", nullable = false)
+    private String zip;
+
+    @Builder
+    public Address(final String address1, final String address2, final String zip) {
+        Assert.hasText(address1, "address1 must not be empty");
+        Assert.hasText(address2, "address2 must not be empty");
+        Assert.hasText(zip, "zip must not be empty");
+
+        this.address1 = address1;
+        this.address2 = address2;
+        this.zip = zip;
+    }
+}
