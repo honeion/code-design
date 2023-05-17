ef9a291753da9c4b47e1e72580dd46d231984882
honeion <honeion2012@gmail.com>
2023-05-18 00:40:31 +0900

    #5 Init

---
+++ ch4-exception/design-4/src/test/java/com/code/design4/Design4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.design4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Design4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
