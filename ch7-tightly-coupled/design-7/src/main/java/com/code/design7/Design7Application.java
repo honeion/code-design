116a92c36680b70480977557caa18f11c93772e5
honeion <honeion2012@gmail.com>
2023-05-20 20:44:07 +0900

    #11 init init

---
+++ ch7-tightly-coupled/design-7/src/main/java/com/code/design7/Design7Application.java
@@ -0,0 +1,13 @@
+package com.code.design7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Design7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Design7Application.class, args);
+	}
+
+}
