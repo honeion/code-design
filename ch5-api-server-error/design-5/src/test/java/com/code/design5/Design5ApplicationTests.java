56f6277c00f49d943ce358dc942d82c2e24bfb77
honeion <honeion2012@gmail.com>
2023-05-20 11:09:27 +0900

    #7 init 기본 셋업

---
+++ ch5-api-server-error/design-5/src/test/java/com/code/design5/Design5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.design5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Design5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
