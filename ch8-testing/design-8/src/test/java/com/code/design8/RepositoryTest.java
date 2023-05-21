99bb8e2c5285222aa8081562282b44d7bc3d9b5a
honeion <honeion2012@gmail.com>
2023-05-21 15:32:55 +0900

    #13 test

---
+++ ch8-testing/design-8/src/test/java/com/code/design8/RepositoryTest.java
@@ -0,0 +1,44 @@
+package com.code.design8;
+
+import com.code.design8.domain.member.Member;
+import com.code.design8.domain.member.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@ActiveProfiles("test")
+@DataJpaTest
+public class RepositoryTest {
+
+    private final MemberRepository memberRepository;
+
+    public RepositoryTest(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Test
+    public void member_test() {
+        //given
+        memberRepository.save(new Member("yun"));
+
+        //when
+        final Member findMember = memberRepository.findByName("yun");
+
+        //then
+        then(findMember.getName()).isEqualTo("yun");
+    }
+
+    @Test
+    @Sql("/member-set-up.sql")
+    public void sql_test() {
+        final List<Member> members = memberRepository.findAll();
+        then(members).hasSize(7);
+    }
+}
